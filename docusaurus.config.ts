361f0551a59554ac1c5651530d17985494f028b9
Furtado Jr., O <ovande.furtado@csun.edu>
2025-04-24 14:41:45 -0700

    Add GitHub Actions workflows for deployment and update baseUrl for GitHub Pages

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,10 +8,10 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-documentation-site.com',
+  url: 'https://wskwellness.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/jkw-guide/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
